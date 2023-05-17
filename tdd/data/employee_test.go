36a1447799274ec324e153ca08970b4df82a6a7e
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-05-17 10:31:01 +0700

    test: add test for new employee

--- tdd/data/employee_test.go
+++ tdd/data/employee_test.go
@@ -1,9 +1,25 @@
 package data_test
 
-import "testing"
+import (
+	"gophertale-tdd/tdd/data"
+	"testing"
+)
+
+// test is the first consumer of the api
 
 func TestEmployeeService(t *testing.T) {
 	t.Run("add new employee", func(t *testing.T) {
+		e := data.Employee{
+			ID:       "A-1",
+			Name:     "Nikita",
+			JobTitle: "The boss of everything",
+		}
 
+		es := data.EmployeeService{}
+		id := es.Add(e)
+
+		if id != e.ID {
+			t.Fatalf("got (%v), want (%v)", id, e.ID)
+		}
 	})
 }
