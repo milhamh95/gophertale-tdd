9755b4501068ea386e6f6def64edb49431ba7fb3
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-05-17 10:26:04 +0700

    feat: initialie employee service

---
+++ tdd/data/employee.go
@@ -0,0 +1,16 @@
+package employees
+
+type EmployeeService struct{}
+
+// use pointer receiver is because we
+// will adding a new employee
+// which will be modifying the data
+// inside employee service
+
+type Employee struct {
+	ID, Name, JobTitle string
+}
+
+func (es *EmployeeService) Add(e Employee) string {
+	return ""
+}
